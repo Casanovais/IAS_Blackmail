e39c1e1b06ce9e657a3516d1d9cc012a87ff94ea
Casanovais <166251657+Casanovais@users.noreply.github.com>
2025-10-30 12:08:37 +0000

    Não sei porque é que não apagou no outro comit

--- verificar_modelos.py
+++ verificar_modelos.py
@@ -3,8 +3,8 @@ import google.generativeai as genai
 import os
 
 # --- 1. CONFIGURAÇÃO ---
-API_KEY = "AIzaSyAVdPUK3n0THFfuhge7QZmEv-GKMhmpmsA" 
-# (Sim, tens de colar a chave aqui também, só para este teste)
+API_KEY = "" 
+
 
 genai.configure(api_key=API_KEY)
 
@@ -20,7 +20,6 @@ try:
             
     print("-------------------------------------------------")
     print("\nCopia um dos nomes de modelo acima (ex: 'models/gemini-pro')")
-    print("E cola-o no teu script 'simulacao_empresa.py' na linha 65.")
 
 except Exception as e:
     print(f"Ocorreu um erro ao listar os modelos: {e}")
